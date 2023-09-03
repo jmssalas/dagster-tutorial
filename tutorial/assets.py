7837cba659d2701cec3e204792c14b9fd670e8af
José María <josemaria.sanchezsalas@gmail.com>
2023-09-03 06:57:00 +0200

    Part 4: Building an asset graph

--- tutorial/assets.py
+++ tutorial/assets.py
@@ -1,8 +1,21 @@
 import json
 import os
 
+import pandas as pd
 import requests
-from dagster import asset
+
+import base64
+from io import BytesIO
+
+import matplotlib.pyplot as plt
+
+from dagster import (
+    AssetExecutionContext,
+    MetadataValue,
+    asset,
+    get_dagster_logger,
+)
+
 
 @asset
 def topstory_ids() -> None:
@@ -12,3 +25,76 @@ def topstory_ids() -> None:
     os.makedirs("data", exist_ok=True)
     with open("data/topstory_ids.json", "w") as f:
         json.dump(top_new_story_ids, f)
+
+
+@asset(deps=[topstory_ids])
+def topstories(context: AssetExecutionContext) -> None:
+    logger = get_dagster_logger()
+
+    with open("data/topstory_ids.json", "r") as f:
+        topstory_ids = json.load(f)
+
+    results = []
+    for item_id in topstory_ids:
+        item = requests.get(
+            f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
+        ).json()
+        results.append(item)
+
+        if len(results) % 20 == 0:
+            logger.info(f"Got {len(results)} items so far.")
+
+    df = pd.DataFrame(results)
+    df.to_csv("data/topstories.csv")
+
+    context.add_output_metadata(
+        metadata={
+            "num_records": len(df),
+            "preview": MetadataValue.md(df.head().to_markdown()),
+        }
+    )
+
+
+@asset(deps=[topstories])
+def most_frequent_words(context: AssetExecutionContext) -> None:
+    stopwords = ["a", "the", "an", "of", "to",
+                 "in", "for", "and", "with", "on", "is"]
+
+    topstories = pd.read_csv("data/topstories.csv")
+
+    # loop through the titles and count the frequency of each word
+    word_counts = {}
+    for raw_title in topstories["title"]:
+        title = raw_title.lower()
+        for word in title.split():
+            cleaned_word = word.strip(".,-!?:;()[]'\"-")
+            if cleaned_word not in stopwords and len(cleaned_word) > 0:
+                word_counts[cleaned_word] = word_counts.get(
+                    cleaned_word, 0) + 1
+
+    # Get the top 25 most frequent words
+    top_words = {
+        pair[0]: pair[1]
+        for pair in sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:25]
+    }
+
+    # Make a bar chart of the top 25 words
+    plt.figure(figsize=(10, 6))
+    plt.bar(top_words.keys(), top_words.values())
+    plt.xticks(rotation=45, ha="right")
+    plt.title("Top 25 Words in Hacker News Titles")
+    plt.tight_layout()
+
+    # Convert the image to a saveable format
+    buffer = BytesIO()
+    plt.savefig(buffer, format="png")
+    image_data = base64.b64encode(buffer.getvalue())
+
+    # Convert the image to Markdown to preview it within Dagster
+    md_content = f"![img](data:image/png;base64,{image_data.decode()})"
+
+    with open("data/most_frequent_words.json", "w") as f:
+        json.dump(top_words, f)
+
+    # Attach the Markdown content as metadata to the asset
+    context.add_output_metadata(metadata={"plot": MetadataValue.md(md_content)})
